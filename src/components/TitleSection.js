6ba0a2ab1e7f307526b3d2c1b62dcd82ba129a14
anuvihas <vihasoffc@gmail.com>
2024-11-16 09:32:52 +0530

    img

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -32,7 +32,7 @@ const TitleSection = () => {
       <div className="container col-xxl-8 px-4 pt-2">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
-            <img className="profile" src="/images/anuvihas.jpg" alt="Anuvihas" />
+            <img className="profile" src="/images/anuvihas.jpeg" alt="Anuvihas" />
           </div>
           <div className="col-lg-6">
             <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Anuvihas S K</h1>
